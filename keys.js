4f3dd5b643ac67832c7e85f51c8d09cafbf2d0f9
amalmazar <amalmazar@gmail.com>
2017-10-19 18:09:15 -0400

    two

---
+++ keys.js
@@ -0,0 +1,11 @@
+JavaScript
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'uJrOc0yXwRiQE8MlzWKWNSg7F',
+  consumer_secret: 'EG8UtyNYhYriuoPZKXOxck5oibPcw7fNuGWG7q7ZH1pynECi7H',
+  access_token_key: '921127357594947585-VyMxN99CvnAv5hfknXttNPkcz2qjOrh',
+  access_token_secret: 'pqWLJSctrUKT2bXRWGvGF9oHNWzSQzZw1eHCZ0bD5SCmF',
+}
+
+module.exports = twitterKeys;
---
+++ liri.js
@@ -0,0 +1,25 @@
+// Spotify codes
+// Client ID: dbe35303c5fd4de7ac5801fe83016cf1
+// Client Secret: 357faeb3a3be4593a948b92e7a84c26c
+
+// 1. Write the code you need to grab the data from keys.js. Then store the keys in a variable.
+// 2. Make it so liri.js can take in one of the following commands: my-tweets, spotify-this-song, movie-this, do-what-it-says
+//    a. my-tweets
+//        Last 20 tweets and when they were created
+//    b. spotify-this-song <song name here>
+//        Song name
+//        Song preview link
+//        Album name
+//        If no song is found, default to "The Sign" by Ace of Base
+//    c. movie-this <movie name here>
+//        Title
+//        Year the movie came out
+//        IMDB rating
+//        Rotten Tomatoes rating
+//        Country where it was produced
+//        Language of the movie
+//        Plot of the movie
+//        Actors in the movie
+//    d. do-what-it-says
+//        Uses the fs node package to run whatever the inside of random.txt tells it to do.
+// 3. Output all the data to a log.txt file. Add to it, don't overwrite it.
